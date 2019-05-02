7fefc7580afd26b463d735e7c2604e53272bc756
Alan Chatfield <47448740+AlanChatfield@users.noreply.github.com>
2019-05-02 10:27:28 +0100

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -254,8 +254,6 @@ $(document).ready( function() {
 	if ($(".eupopup").length > 0) {
 		$(document).euCookieLawPopup().init({
 			'info' : 'YOU_CAN_ADD_MORE_SETTINGS_HERE',
-			'popupTitle' : 'This website is using cookies. ',
-			'popupText' : 'We use them to give you the best experience. If you continue using our website, we\'ll assume that you are happy to receive all cookies on this website.'
 		});
 	}
 });
